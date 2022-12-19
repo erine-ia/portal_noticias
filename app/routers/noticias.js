062547362b7fb58c5f2f701d1baefc40ebc3db44
ed <erineia@live.com>
2022-12-19 12:40:13 -0300

    estabelecendo conexões

--- app/routers/noticias.js
+++ app/routers/noticias.js
@@ -1,9 +1,22 @@
 
-
 module.exports = function(app){
-   app.get('/noticias', function(req, res){
-        res.render('noticias/noticias')
+    app.get('/noticias', function(req, res){
+        const mysql = require('mysql')
+        var connection  = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'password',
+            database : 'portal_noticias'
+        });
 
-    }) 
-    return 
+
+        connection.query('SELECT * FROM noticias', (erro, resultado)=>{
+            res.render('noticias/noticias', {noticias:resultado});
+            
+        })
+        
+           // res.render('noticias/noticias')
+    })
+
+    
 }
--- config/server.js
+++ config/server.js
@@ -5,4 +5,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views')
 
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 module.exports = app
